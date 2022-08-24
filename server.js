cbcc0452d0c8cbfd8b1a72909a44c9fe87d38b19
Jung Andy <jungj0925@gmail.com>
2022-08-24 17:24:08 -0400

    fixerd port and old ref db

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.put('/image', (req, res) => {handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => {handleApiCall(req, res)})
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server working');
 })
 
