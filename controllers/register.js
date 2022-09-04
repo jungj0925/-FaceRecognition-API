8d2b16fc4f591d9f4fce2e93ddf9c7459bc6c27c
Jung Andy <jungj0925@gmail.com>
2022-09-04 07:06:25 -0400

    fixing database

--- controllers/register.js
+++ controllers/register.js
@@ -29,7 +29,7 @@ const handleRegister = (req, res, db, bcrypt) => {
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('Unable to Register'))
+    .catch((err) => res.status(400).json(err));
 }
 
 export {handleRegister}
