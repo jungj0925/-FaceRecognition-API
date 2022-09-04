be99fc120d302cc3004966a19dc0046e5107c4bb
Jung Andy <jungj0925@gmail.com>
2022-09-04 06:33:00 -0400

    deploying database

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ import {handleImage, handleApiCall} from './controllers/image.js';
 const db = knex ({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-closed-48980',
         port: '5432',
         user: 'postgres',
-        password: '090327ha',
+        password: '',
         database: 'facerecog'
     }
 })
